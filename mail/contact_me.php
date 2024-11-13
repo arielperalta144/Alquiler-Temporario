f8eb5453b833c294b0f8f34983f82869af365133
Ariel <62582408+arielperalta144@users.noreply.github.com>
2024-11-13 14:50:14 -0300

    se actualiza

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,24 +1,25 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
+if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+   echo "¡No se proporcionan argumentos!";
+   return false;
+}
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>
+
+// Crea el correo electrónico y envía el mensaje
+$to = 'susanacasadeycba@gmail.com'; // Dirección de correo donde recibes el mensaje
+$email_subject = "Formulario de contacto del sitio web: $contact_me";
+$email_body = "Ha recibido un nuevo mensaje desde el formulario de contacto de su sitio web.\n\n" . "Aquí están los detalles:\n\nNombre: $name\n\nCorreo electrónico: $email_address\n\nMensaje:\n$message";
+$headers = "From: susanacasadeycba@susanacasadey.com.ar\n"; // Cambia esto por un correo válido desde tu dominio
+$headers .= "Reply-To: $email_address";
+
+if (mail($to, $email_subject, $email_body, $headers)) {
+   echo "¡Mensaje enviado con éxito!";
+   return true;
+} else {
+   echo "Error al enviar el mensaje.";
+   return false;
+}
